07fd75116851f8a5ab9ca056153755b10fae09c2
rachinkapoor <rachin@ost.com>
2019-03-03 23:22:52 +0530

    Merge branch 'develop' into init-291118

---
+++ ostsdk/src/main/java/com/ost/mobilesdk/security/SignedRestRecoveryStruct.java
@@ -0,0 +1,41 @@
+package com.ost.mobilesdk.security;
+
+import org.json.JSONObject;
+
+public class SignedRestRecoveryStruct {
+
+    private String recoveryContractAddress;
+    private String signature;
+    private String newRecoverOwnerAddress;
+    private String recoveryOwnerAddress;
+    private JSONObject eip712TypedData;
+
+    public SignedRestRecoveryStruct(String newRecoverOwnerAddress, String recoveryOwnerAddress, String recoveryContractAddress, JSONObject typedData, String signature) {
+        this.newRecoverOwnerAddress = newRecoverOwnerAddress;
+        this.recoveryOwnerAddress = recoveryOwnerAddress;
+        this.recoveryContractAddress = recoveryContractAddress;
+        this.eip712TypedData = typedData;
+        this.signature = signature;
+    }
+
+    public String getSignature() {
+        return signature;
+    }
+
+    public String getNewRecoverOwnerAddress() {
+        return newRecoverOwnerAddress;
+    }
+
+    public String getRecoveryOwnerAddress() {
+        return recoveryOwnerAddress;
+    }
+
+    public String getRecoveryContractAddress() {
+        return recoveryContractAddress;
+    }
+
+    public JSONObject getEip712TypedData() {
+        return eip712TypedData;
+    }
+
+}
