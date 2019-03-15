cfc7c7f246e98b8bc64be5a304271f4f7935e45f
sachinchauhan <sachin@pepo.com>
2019-03-15 19:52:35 +0530

    Copyright update of all the ostmobilesdk files Removed backup_rules unused file

--- ostsdk/src/main/java/com/ost/mobilesdk/biometric/OstBiometricAuthentication.java
+++ ostsdk/src/main/java/com/ost/mobilesdk/biometric/OstBiometricAuthentication.java
@@ -1,14 +1,20 @@
+/*
+ * Copyright 2019 OST.com Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.ost.mobilesdk.biometric;
 
-import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.v4.content.LocalBroadcastManager;
-import android.util.Log;
 
 public class OstBiometricAuthentication {
     public static final String INTENT_FILTER_FINGERPRINT_AUTH = "com.ost.ostsdk.biometric.FINGERPRINT_AUTH";
--- ostsdk/src/main/java/com/ost/mobilesdk/database/OstSdkKeyDatabase.java
+++ ostsdk/src/main/java/com/ost/mobilesdk/database/OstSdkKeyDatabase.java
@@ -1,10 +1,19 @@
+/*
+ * Copyright 2019 OST.com Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ */
+
 package com.ost.mobilesdk.database;
 
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
-import android.util.Log;
 
 import com.ost.mobilesdk.database.daos.OstSecureKeyDao;
 import com.ost.mobilesdk.models.entities.OstSecureKey;
