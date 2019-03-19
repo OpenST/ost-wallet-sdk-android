8e3c69db5fc67d52fa3d5795d434efdad418d723
JunishaM <33958778+JunishaM@users.noreply.github.com>
2019-03-19 20:42:50 +0530

    Error Messages
    Done till line 125. :)

--- ostsdk/src/main/java/com/ost/walletsdk/workflows/errors/OstErrors.java
+++ ostsdk/src/main/java/com/ost/walletsdk/workflows/errors/OstErrors.java
@@ -19,109 +19,110 @@ public class OstErrors {
 
     public static String getMessage(ErrorCode code) {
         switch (code) {
-            case INVALID_USER_ID: return "Invalid user Id";
-            case INVALID_WORKFLOW_CALLBACK: return "Invalid Workflow Callback. Workflow callback can not be null.";
+            case INVALID_USER_ID: return "Unable to recognize the user id. Please inspect for what is being sent, rectify and re-submit.";
+            case INVALID_WORKFLOW_CALLBACK: return "Callback is essential for a workflow to continue running, it cannot be black.";
             case KIT_API_ERROR: return "Kit Api returned error.";
             case GET_USER_API_FAILED:
-                return "Failed to fetch user information";
+                return "Failed to fetch user information. Either OST server is unavailable temporarily OR your connection is going idle. Check your connection and re-submit the request a bit later.";
 
             case SALT_API_FAILED:
-                return "Failed to fetch user salt";
+                return "Failed to fetch user salt. Either OST server is unavailable temporarily OR your connection is going idle. Check your connection and re-submit the request a bit later.";
 
             case TOKEN_API_FAILED:
-                return "Failed to fetch token information";
+                return "Failed to fetch token information. Either OST server is unavailable temporarily OR your connection is going idle. Check your connection and re-submit the request a bit later.";
 
             case CHAIN_API_FAILED:
-                return "Failed to fetch block-chain information";
+                return "Failed to fetch block-chain information. Either OST server is unavailable temporarily OR your connection is going idle. Check your connection and re-submit the request a bit later.";
 
-            case RULES_API_FAILED:return "Failed to fetch rule information";
+            case RULES_API_FAILED:return "Failed to fetch rule information. Either OST server is unavailable temporarily OR your connection is going idle. Check your connection and re-submit the request a bit later.";
 
-            case GET_DEVICE_API_FAILED: return "Failed to fetch device Information";
+            case GET_DEVICE_API_FAILED: return "Failed to fetch device Information. Either OST server is unavailable temporarily OR your connection is going idle. Check your connection and re-submit the request a bit later.";
 
-            case DEVICE_MANAGER_API_FAILED: return "Failed to fetch device manager information";
+            case DEVICE_MANAGER_API_FAILED: return "Failed to fetch device manager information. Either OST server is unavailable temporarily OR your connection is going idle. Check your connection and re-submit the request a bit later.";
 
             case INVALID_WORKFLOW_PARAMS:
-                return "Invalid workflow params";
+                return "Invalid workflow params. Please ensure the input is well formed or visit https://dev.ost.com/platform/docs/sdk/references for details on workflow parameters.
+";
 
             case CREATE_DEVICE_FAILED:
-                return "Failed to create device.";
+                return "Failed to create device. Please inspect the input is well formed and re-try.";
 
             case ACTIVATE_USER_API_FAILED:
-                return "Something went wrong while activating user";
+                return "Unable to activate the user. Inspect if correct input values are being sent, the input is well formed and re-try. If the problem persists contact support@ost.com .";
 
             case ACTIVATE_USER_API_POLLING_FAILED:
-                return "Something went wrong while activating user";
+                return "Unable to complete the user activation flow. Inspect if correct input values are being sent, the input is well formed and re-try. If the problem persists contact support@ost.com . ";
 
             case DEVICE_NOT_SETUP:
-                return "Device not registered";
+                return "Unable to recognize the device. Please setup this device for the user using workflow provided at https://dev.ost.com/platform/docs/sdk/references";
 
             case USER_NOT_ACTIVATED:
-                return "User not activated";
+                return "The user is not activated yet. Please setup user's wallet to enable their participation in your economy. ";
 
             case POLLING_TIMEOUT:
-                return "Polling timeout";
+                return "Polling timeout. This can be intermittent event with a request failing followed by successful one.";
 
             case ADD_DEVICE_API_FAILED:
-                return "Add device api failed";
+                return "Add device api failed. Either OST server is unavailable temporarily OR The API request object sent cannot be executed. Please re-try and if the problem persists contact support@ost.com .";
 
             case BLOCK_NUMBER_API_FAILED:
-                return "Block number api failed";
+                return "Block number api failed. Either OST server is unavailable temporarily OR The API request object sent cannot be executed. Please re-try and if the problem persists contact support@ost.com . ";
 
             case EIP712_FAILED:
-                return "EIP712 failed";
+                return "Unable to sign parameters using EIP 712 and verify the signature.";
 
             case NO_SESSION_FOUND:
-                return "No Session found";
+                return "The device doesn't has any active session. Please authorize a session before doing any transaction. Workflow details provided at https://dev.ost.com/platform/docs/sdk/references ";
 
             case TRANSACTION_API_FAILED:
-                return "Transaction api failed";
+                return "Transaction api failed. Either OST server is unavailable temporarily OR The API request object sent cannot be executed. Please re-try and if the problem persists contact support@ost.com .";
 
             case RULE_NOT_FOUND:
-                return "Rule not found";
+                return "Unable to recognize the Rule. Please inspect a valid rule name is passed and re-submit the request. ";
 
             case DIFFERENT_ECONOMY:
-                return "Token Id is different";
+                return "You are not authorized to access the data of the economy you are trying to fetch. Inspect if a correct value is being sent in token Id field and re-submit the request.";
 
             case USER_NOT_FOUND:
-                return "User not found";
+                return "Unable to find this user in your economy. Inspect if a correct value is being sent in user Id field and re-submit the request. ";
 
             case CURRENT_DEVICE_NOT_FOUND:
-                return "Current device for user not found";
+                return "Current device is not registered with the user. Either rectify the value being sent in device Id field OR register this device with the user. ";
 
             case POLLING_API_FAILED:
-                return "Polling api failed";
+                return "Polling api failed. This can be intermittent event with a request or two failing followed by successful one.";
 
             case UNKNOWN_ENTITY_TYPE:
-                return "Unknown entity type";
+                return "Unable to recognize the API request object sent and so cannot be executed.";
 
             case INVALID_QR_DEVICE_OPERATION_DATA:
-                return "Invalid qr device operation data";
+                return "The QR code for adding a new device is not well formed. To know the QR code data formation details based on type of operations please visit https://dev.ost.com/platform ";
 
             case INVALID_ADD_DEVICE_ADDRESS:
-                return "Invalid add device address";
+                return "Invalid add device address. Please ensure the input is well formed or visit https://dev.ost.com/platform/docs/api for details on accepted datatypes for API parameters.";
 
             case INVALID_RECOVER_DEVICE_ADDRESS:return "Invalid device address. This address can not be recovered.";
 
             case DEVICE_UNAUTHORIZED:
-                return "Device unauthorized";
+                return "Unable to perform the operation as the device not unauthorized. For details on how to authorize a device please visit https://dev.ost.com/platform/docs/sdk/references ";
 
             case DEVICE_ALREADY_AUTHORIZED:
-                return "Device already authorized";
+                return "This Device is already authorized";
 
             case USER_ALREADY_ACTIVATED:
-                return "User already activated";
+                return "The User is already activated";
 
             case INVALID_MNEMONICS:
-                return "Mnemonics are invalid";
+                return "The 12 word passphrase you provided is incorrect. ";
 
             case INVALID_QR_TRANSACTION_DATA:
-                return "Invalid qr transaction data";
+                return "The QR code for executing a transaction is not well formed. To know the QR code data formation details based on type of operations please visit https://dev.ost.com/platform";
 
             case INVALID_USER_PASSPHRASE:
-                return "Pin is Invalid";
+                return "The 6 digit PIN you entered is not correct.";
 
             case INVALID_NEW_USER_PASSPHRASE:
-                return "Pin is Invalid";
+                return "The new 6 digit PIN you entered is not correct.";
 
 
             case MAX_PASSPHRASE_VERIFICATION_LIMIT_REACHED:
