5ce17ac235b907501dea138738df45bae3c011f5
pankaj <pankaj@ost.com>
2019-06-04 22:26:10 +0530

    Send Tokens page change

--- demoapp/src/main/java/com/ost/ostwallet/App.java
+++ demoapp/src/main/java/com/ost/ostwallet/App.java
@@ -12,7 +12,6 @@ package com.ost.ostwallet;
 
 import android.app.Application;
 
-import com.ost.ostwallet.entity.CurrentEconomy;
 import com.ost.walletsdk.OstSdk;
 
 import java.net.CookieHandler;
@@ -21,8 +20,6 @@ import java.net.CookiePolicy;
 import java.net.CookieStore;
 
 import com.ost.ostwallet.network.PersistentCookieStore;
-
-import org.json.JSONException;
 
 public class App extends Application {
 
@@ -37,14 +34,6 @@ public class App extends Application {
 
         AppProvider.init(getApplicationContext());
         AppProvider.get().setPersistentCookieStore(persistentCookieStore);
-
-        CurrentEconomy currentEconomy;
-        try {
-            currentEconomy = CurrentEconomy.newInstance("{\"token_id\":1141,\"token_name\":\"T21053\",\"token_symbol\":\"T213\",\"url_id\":\"7f77d96c05c1c18c8b42cf537a2676653d7d99ffff2d013152fb4f402b7e8e23\",\"mappy_api_endpoint\":\"https://demo-mappy.stagingost.com/demo/\",\"saas_api_endpoint\":\"https://api.stagingost.com/testnet/v2/\",\"view_api_endpoint\":\"https://view.stagingost.com/testnet/\"}");
-        } catch (JSONException e) {
-            throw  new RuntimeException("In App");
-        }
-        AppProvider.get().setCurrentEconomy(currentEconomy);
 
         if (null != AppProvider.get().getCurrentEconomy()) {
             OstSdk.initialize(getApplicationContext(), AppProvider.get().getCurrentEconomy().getSaasApiEndpoint());
