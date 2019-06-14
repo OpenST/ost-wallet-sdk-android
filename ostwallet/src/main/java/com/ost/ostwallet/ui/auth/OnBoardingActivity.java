cf27a554d5569cd2269c12e7bce25d5259e8f2c7
sachinchauhan <sachin@pepo.com>
2019-06-14 16:27:27 +0530

    OstSdk: Added CrashAnalytics

--- ostwallet/src/main/java/com/ost/ostwallet/ui/auth/OnBoardingActivity.java
+++ ostwallet/src/main/java/com/ost/ostwallet/ui/auth/OnBoardingActivity.java
@@ -44,7 +44,7 @@ public class OnBoardingActivity extends BaseActivity implements
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Fabric.with(this, new Crashlytics());
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_onboarding);
 
         mOnBoardingPresenter.attachView(this);
