e6d92c86dbefb6f362112206c41051dc7208de71
sachinchauhan <sachin@pepo.com>
2019-09-04 16:34:02 +0530

    Added trustkit network_config Exception handling

--- ostsdk/src/main/java/com/ost/walletsdk/OstSdk.java
+++ ostsdk/src/main/java/com/ost/walletsdk/OstSdk.java
@@ -17,6 +17,7 @@ import android.support.annotation.Nullable;
 import android.util.Log;
 
 import com.datatheorem.android.trustkit.TrustKit;
+import com.datatheorem.android.trustkit.config.ConfigurationException;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import com.ost.walletsdk.database.OstSdkDatabase;
 import com.ost.walletsdk.database.OstSdkKeyDatabase;
@@ -143,6 +144,8 @@ public class OstSdk {
                 TrustKit.initializeWithNetworkSecurityConfiguration(mApplicationContext, R.xml.ost_network_security_config);
             } catch (IllegalStateException exception) {
                 // Already initialized by app.
+            } catch (ConfigurationException exception) {
+                // Configuration is different because app is initializing with different config
             }
             mTrustKitInitialised = true;
         }
