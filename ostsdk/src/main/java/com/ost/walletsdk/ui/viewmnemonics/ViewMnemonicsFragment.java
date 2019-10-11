0444857bffb12a3e9f474892e6495dab909d3c68
sachinchauhan <sachin@pepo.com>
2019-10-11 19:51:22 +0530

    mnemonics export

--- ostsdk/src/main/java/com/ost/walletsdk/ui/viewmnemonics/ViewMnemonicsFragment.java
+++ ostsdk/src/main/java/com/ost/walletsdk/ui/viewmnemonics/ViewMnemonicsFragment.java
@@ -12,9 +12,15 @@ package com.ost.walletsdk.ui.viewmnemonics;
 
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
@@ -26,6 +32,11 @@ import com.ost.walletsdk.ui.uicomponents.uiutils.content.ContentConfig;
 import com.ost.walletsdk.ui.uicomponents.uiutils.content.StringConfig;
 
 import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -87,9 +98,78 @@ public class ViewMnemonicsFragment extends BaseFragment {
                 StringConfig.instance(contentConfig.optJSONObject("bottom_label")).getString()
         );
 
+        labelCaution.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createMnemonicsFile();
+            }
+        });
+
         AppBar appBar = AppBar.newInstance(getContext(), false);
         setUpAppBar(viewGroup, appBar);
         showMnemonics(mnemonics);
+    }
+
+    private void createMnemonicsFile() {
+        int width = 200;
+        int height = 150;
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.WHITE);
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawPaint(paint);
+
+        paint.setColor(Color.BLACK);
+        paint.setAntiAlias(true);
+        paint.setTextSize(9.f);
+
+        String[] arr = mnemonics.split(" ");
+
+        float text_height = paint.measureText(arr[0]);
+        float text_width = paint.measureText(arr[0]);
+        float x_coord = ((bitmap.getWidth()/2) - 3 * text_width);
+        float x_coord_offset = ((bitmap.getWidth()/2) + text_width);
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i < 6) {
+                if (i == 5) {
+                    canvas.drawText(String.format("%s. %s", i + 1, arr[i]), x_coord, text_height * (i + 1), paint);
+                } else {
+                    canvas.drawText(String.format("%s. %s", i + 1, arr[i]), x_coord, text_height * (i + 1), paint);
+                }
+            } else {
+                if (i == 11) {
+                    canvas.drawText(String.format("%s. %s", i + 1, arr[i]), x_coord_offset, text_height * (i - 5), paint);
+                } else {
+                    canvas.drawText(String.format("%s. %s", i + 1, arr[i]), x_coord_offset, text_height * (i - 5), paint);
+                }
+            }
+        }
+
+        File f = new File(getBaseActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES),"mnemonics.jpg");
+        try {
+            f.createNewFile();
+
+
+            //Convert bitmap to byte array
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.PNG, 0 /*ignored for PNG*/, bos);
+            byte[] bitmapdata = bos.toByteArray();
+
+            //write the bytes in file
+            FileOutputStream fos = new FileOutputStream(f);
+            fos.write(bitmapdata);
+            fos.flush();
+            fos.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        showToastMessage(String.format("12 Words file at %s",f.getAbsolutePath()),true);
     }
 
 
